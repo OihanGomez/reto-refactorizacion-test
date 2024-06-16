0da200705f18c048c98cf5bf48ea63f5e9e29272
OihanGomez <ogomezaz23dw@ikzubirimanteo.com>
2024-06-16 21:36:06 +0200

    Documentation with javadoc in CitasManager

--- src/Libreria/Acciones/CitasManager.java
+++ src/Libreria/Acciones/CitasManager.java
@@ -1,20 +1,32 @@
 package Libreria.Acciones;
 
 import Libreria.objetos.Cita;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+/**
+ * Clase que gestiona las operaciones relacionadas con las citas en la librería.
+ */
 public class CitasManager {
     private ConexionBD conexionBD;
 
+    /**
+     * Constructor de la clase CitasManager.
+     * Inicializa la conexión a la base de datos mediante un objeto ConexionBD.
+     */
     public CitasManager() {
         conexionBD = new ConexionBD();
     }
 
+    /**
+     * Reserva una cita para un usuario en la base de datos.
+     *
+     * @param cita Objeto Cita que contiene la información de la cita a reservar.
+     * @throws Exception Si la fecha de la cita ya está ocupada para el usuario especificado.
+     */
     public void reservarCita(Cita cita) throws Exception {
         Connection connection = conexionBD.getConexion();
         String checkQuery = "SELECT COUNT(*) FROM CITAS WHERE EMAIL = ? AND FECHA = ?";
@@ -39,6 +51,13 @@ public class CitasManager {
         }
     }
 
+    /**
+     * Verifica si una fecha específica está ocupada para citas en la base de datos.
+     *
+     * @param fecha Fecha para la cual se desea verificar la disponibilidad.
+     * @return true si la fecha está ocupada por al menos una cita, false si está disponible.
+     * @throws SQLException Si ocurre un error al ejecutar la consulta SQL.
+     */
     public boolean isFechaOcupada(LocalDate fecha) throws SQLException {
         Connection connection = conexionBD.getConexion();
         String query = "SELECT COUNT(*) FROM CITAS WHERE FECHA = ?";
