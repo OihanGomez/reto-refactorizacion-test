a82dfe12721fc50c1d4d7a3b9fb9ca888f346497
OihanGomez <ogomezaz23dw@ikzubirimanteo.com>
2024-06-16 12:06:42 +0200

    AdminManagement documentation with javadoc.

--- src/Libreria/Acciones/AdminManagement.java
+++ src/Libreria/Acciones/AdminManagement.java
@@ -6,14 +6,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JComboBox;
 
+/**
+ * La clase AdminManagement se encarga de la gestión de la pagina de la administracion.
+ */
 public class AdminManagement {
-    private  Connection conexion;
+    private Connection conexion;
 
+    /**
+     * Constructor de la clase AdminManagement.
+     *
+     * @param conexion Objeto de tipo ConexionBD que proporciona la conexión a la base de datos.
+     */
     public AdminManagement(ConexionBD conexion) {
         this.conexion = conexion.getConexion();
     }
 
-
+    /**
+     * Actualiza un JComboBox con los usuarios de la base de datos.
+     *
+     * @param comboBox El JComboBox donde se agregarán los correos electrónicos de los usuarios.
+     */
     public void mostrarUsuarios(JComboBox<String> comboBox) {
         try {
             PreparedStatement statement = conexion.prepareStatement("SELECT email FROM usuarios");
@@ -27,7 +39,16 @@ public class AdminManagement {
         }
     }
 
-
+    /**
+     * Actualiza la información de un usuario en la base de datos.
+     *
+     * @param selectedUser El correo electrónico del usuario seleccionado.
+     * @param nuevaDireccion La nueva dirección del usuario.
+     * @param nuevoNombre El nuevo nombre del usuario.
+     * @param nuevosApellidos Los nuevos apellidos del usuario.
+     * @param nuevoCorreo El nuevo correo electrónico del usuario.
+     * @param nuevaContrasena La nueva contraseña del usuario.
+     */
     public void actualizarUsuario(String selectedUser, String nuevaDireccion, String nuevoNombre, String nuevosApellidos, String nuevoCorreo, String nuevaContrasena) {
         try {
             PreparedStatement statement = conexion.prepareStatement("UPDATE usuarios SET direccion = ?, nombre = ?, apellidos = ?, email = ?, contrasena = ? WHERE email = ?");
